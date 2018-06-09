eaf38588457f92c1370ab3be88abe7e0b76ab5b4
Gegel85 <Gegel85@laposte.net>
2018-06-10 01:19:48 +0200

    Fixed crash when hitting Blitz's Q
    No longer crash the server when hitting something with Blitz's Grab. The target is teleposted on Blitz. However, the target doesn't appear on him if it doesn't do any action like moving casting spell, ect...

--- Champions/Blitzcrank/Q.cs
+++ Champions/Blitzcrank/Q.cs
@@ -34,17 +34,13 @@ namespace Spells
         {
             var ap = owner.GetStats().AbilityPower.Total;
             var damage = 25 + spell.Level * 55 + ap;
-            spell.Target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
+            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
                 false);
-            if (!spell.Target.IsDead)
+            ApiFunctionManager.LogInfo("Before if " + target);
+            if (!target.IsDead)
             {
-                ApiFunctionManager.AddParticleTarget(owner, "Blitzcrank_Grapplin_tar.troy", spell.Target, 1, "L_HAND");
-                var current = new Vector2(owner.X, owner.Y);
-                var to = Vector2.Normalize(new Vector2(spell.X, spell.Y) - current);
-                var range = to * 50;
-                var trueCoords = current + range;
-                ApiFunctionManager.DashToLocation((ObjAIBase) spell.Target, trueCoords.X, trueCoords.Y,
-                    spell.SpellData.MissileSpeed, true);
+                ApiFunctionManager.AddParticleTarget(owner, "Blitzcrank_Grapplin_tar.troy", target, 1, "L_HAND");
+                target.setPosition(owner.X, owner.Y);
             }
 
             projectile.setToRemove();
