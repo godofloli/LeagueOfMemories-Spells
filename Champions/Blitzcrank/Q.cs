80a73476376dcde0fa1bfa73c5d854c23ac8ab8a
Andgel <andgel.halley@epitech.eu>
2018-06-12 15:25:07 +0200

    Put back old grab but changed spell.Traget to target

--- Champions/Blitzcrank/Q.cs
+++ Champions/Blitzcrank/Q.cs
@@ -36,11 +36,15 @@ namespace Spells
             var damage = 25 + spell.Level * 55 + ap;
             target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
                 false);
-            ApiFunctionManager.LogInfo("Before if " + target);
             if (!target.IsDead)
             {
                 ApiFunctionManager.AddParticleTarget(owner, "Blitzcrank_Grapplin_tar.troy", target, 1, "L_HAND");
-                target.setPosition(owner.X, owner.Y);
+                var current = new Vector2(owner.X, owner.Y);
+                var to = Vector2.Normalize(new Vector2(spell.X, spell.Y) - current);
+                var range = to * 50;
+                var trueCoords = current + range;
+                ApiFunctionManager.DashToLocation((ObjAIBase)target, trueCoords.X, trueCoords.Y,
+                    spell.SpellData.MissileSpeed, true);
             }
 
             projectile.setToRemove();
